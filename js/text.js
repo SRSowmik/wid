686238f2e5589d98186665646b25d90e57a84117
srsowmik <srsowmikhost@gmail.com>
2021-04-14 16:28:45 +0600

    first commit

---
+++ js/text.js
@@ -0,0 +1,10 @@
+   
+  var typed = new Typed("#typed", {
+    stringsElement: '#typed-strings',
+    typeSpeed: 40,
+    backSpeed: 40,
+    backDelay: 500,
+    startDelay: 1000,
+    loop: true,
+  });
+
